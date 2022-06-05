e20c3fc269b0052b92294f297ccf5eeb9ee587cd
Lee.t.c <seonghoo1217@naver.com>
2022-06-05 13:22:00 +0900

    CI_CD settings

---
+++ src/main/java/learn/cicd/springboot_ci_cd/SpringBootCiCdApplication.java
@@ -0,0 +1,13 @@
+package learn.cicd.springboot_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/cicd/springboot_ci_cd/SpringBootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.cicd.springboot_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
